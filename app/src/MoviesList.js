510950b8afad29199a34ac9bb6161a3ccafd4d45
edufmass <eduardo@massolini.com.ar>
2023-11-02 10:33:51 -0300

    added dom router

---
+++ app/src/MoviesList.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import Movie from './Movie';
+import PageWrapper from './PageWrapper';
+import movieListJson from './movies.json';
+import Pagination from './Pagination';
+
+export default function MoviesList() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [movieList, setMovieList] = useState([]);
+  const ITEMS_PER_PAGE = 3;
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const getMovies = async () => {
+    let url = 'api/movies.json';
+    let response = await fetch(url, {
+      "method": "GET",
+      "mode": "no-cors",
+      "headers": {
+        "Accept": "application/json",
+        "Content-type": "application/json"
+      }
+    });
+    let jsonResponse = await response.json();
+    setMovieList(jsonResponse);
+  }
+
+
+  const getMoviesPerPage = () => {
+    return movieList.slice(
+      (currentPage-1) * ITEMS_PER_PAGE,
+      currentPage * ITEMS_PER_PAGE
+    );
+  }
+
+  const getTotalPages = () => {
+    let moviesTotal = movieListJson.length;
+    return Math.ceil(moviesTotal / ITEMS_PER_PAGE);
+  }
+
+  return (
+    <PageWrapper>
+
+      {/* without { } the first element is returned, with { } return is needed */}
+      {getMoviesPerPage().map(movie => 
+          <Movie
+          movieImg={movie.img}
+          movieTitle={movie.title}
+          movieYear={movie.year}
+          movieRate={movie.rate}
+          movieDuration={movie.duration}
+          movieMMPA={movie.mmpa}
+          movieRelease={movie.release}
+          movieDirector={movie.director}
+          movieStars={movie.stars}
+          >
+            {movie.description}            
+        </Movie>
+      )};
+
+      <Pagination
+        current={currentPage}
+        total={getTotalPages()}
+        pageChange={(page) => {
+          setCurrentPage(page);
+        }}
+       />
+      
+    </PageWrapper>
+  );
+}
